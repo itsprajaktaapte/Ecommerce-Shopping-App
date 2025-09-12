62bdc032ce16888432978e52b9307efd969b8aae
Prajaktaapte91 <itsprajaktaapte@gmail.com>
2025-09-12 18:58:29 +1200

    Third Commit
    Beautifying home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,9 @@ import React from "react";
 import banner from "../assets/images/banner.webp"
 const Home = () => {
   return (
-    <div className="p-6 text-center ">
-      <h1 className="text-3xl font-bold text-green-900">Welcome Shoppers</h1>
-      <p className="mt-4 text-lg">Best deals on clothes, jewelry, electronics and more!</p>
+    <div className="p-6 text-center bg-gradient-to-r from-blue-300 to-green-200 w-full h-screen ">
+      <h1 className="text-5xl font-bold text-green-900">Welcome Shoppers</h1>
+      <p className="mt-4 text-2xl">Best deals on Clothes, Jewelry, Electronics and more!</p>
       
       {/* Ads */}
       <div className="mt-6 flex justify-center">
