611a6132874f3a680f0d45ea536731ed7301823d
Prajaktaapte91 <itsprajaktaapte@gmail.com>
2025-09-22 12:20:57 +1200

    Commit- Name change
    E-commerce app name change

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     <div className=" max-w-screen bg-gray-900 text-white flex items-center justify-between px-6 py-3">
       {/* Logo */}
       <img className=" h-10" src={logo} alt="Company Logo" />
-      <h3 className="text-white p-4 flex justify-start text-3xl ">Shopper's Stop</h3>
+      <h3 className="text-white p-4 flex justify-start text-3xl ">Prajakta's E-Store</h3>
       <nav className="w-3/4 text-white p-4 flex justify-end gap-8 shadow-md">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
